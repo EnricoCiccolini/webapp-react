3adc01a3ba6c1613da92f80005403f55039b98a8
EnricoCiccolini <piroroma90@gmail.com>
2025-05-12 13:49:06 +0200

    feat : mileston 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
-      main app
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<div> home page</div>} />
+          <Route path="/:id" element={<div> dettaglio del film</div>} />
+
+
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
