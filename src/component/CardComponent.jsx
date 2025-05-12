dc745daf1150cb7ebc609e84225e7fbe176384ca
EnricoCiccolini <piroroma90@gmail.com>
2025-05-12 17:21:57 +0200

    feat : add star for vote

--- src/component/CardComponent.jsx
+++ src/component/CardComponent.jsx
@@ -2,6 +2,19 @@ import { Link } from "react-router-dom";
 
 function CardComponent({ film }) {
 
+    function star(vote) {
+
+        const stelle = [];
+        for (let index = 0; index < 5; index++) {
+            if (index < vote) {
+                stelle.push(<i key={index} className="fa-solid fa-star" style={{ color: '#FFD43B' }}></i>);
+            } else {
+                stelle.push(<i key={index} className="fa-light fa-star"></i>);
+            }
+        }
+        return stelle;
+    }
+
 
 
     const { id, title, director, genre, release_year, abstract, review_vote, imagepath } = film;
@@ -12,11 +25,11 @@ function CardComponent({ film }) {
                 <img src={imagepath} className="w-50 mx-auto" alt={title} />
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
-                    <p className="card-text"><strong>{director}</strong></p>
-                    <p className="card-text">{abstract}</p>
-                    <p className="card-text">{genre}</p>
-                    <p className="card-text">{release_year}</p>
-                    <p className="card-text">{review_vote}</p>
+                    <p className="card-text">regista: <strong>{director}</strong></p>
+                    <p className="card-text">trama: {abstract}</p>
+                    <p className="card-text">genere: {genre}</p>
+                    <p className="card-text">anno di uscita: {release_year}</p>
+                    <p className="card-text">voto medio: {star(review_vote)}</p>
                     <Link to={`/${id}`} className="btn btn-primary">vai al dettaglio </Link>
                 </div>
             </div>
